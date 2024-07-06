aee432bb67028a023150af1e6647d9f583b8737e
DevAlper35 <devcalisma@gmail.com>
2024-07-07 01:25:24 +0300

    Create commit

---
+++ app/src/main/java/com/mudurlu/lifecycle/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.mudurlu.lifecycle
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //onCreate oluşum
+        println("onCreate çalıştı")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //onCreate ve onResume arası bir bölüm
+        println("onStart çalıştı")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Kullanıcının ekranına gelen kısım
+        println("onResume çalıştı")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //Uygulamadan çıkınca ilk pause çalışır
+        println("onPause çalıştı")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //Uygulamadan çıkınca ikinci pause çalışır
+        println("onStop çalıştı")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy çalıştı")
+    }
+
+
+}
